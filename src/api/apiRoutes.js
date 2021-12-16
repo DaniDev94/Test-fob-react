8399654f5e38b5c13d7b44961730ea07219996e9
Daniel López <daniel.lp1094@gmail.com>
2021-12-16 14:18:25 +0100

    REFACTOR: remove apiRoutes.js file and fetch without axios

--- src/api/apiRoutes.js
+++
@@ -1,1 +0,0 @@
-const BASE_URL = "https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0";
--- src/api/infoApi.js
+++ src/api/infoApi.js
@@ -1,10 +1,22 @@
-import { BASE_URL } from './apiRoutes'
-import axios from "axios";
+// import axios from "axios";
+
+
+const config = {
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    }
+}
 
 export const getAllInfo = async () => {
     try {
-        const req = await axios.get(BASE_URL);
-        return req;
+        const req = await fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0", {
+            method: "GET",
+            headers: config.headers,
+        });
+        const res = req.json();
+        return res;
     } catch (err) {
         console.log(err)
     }
