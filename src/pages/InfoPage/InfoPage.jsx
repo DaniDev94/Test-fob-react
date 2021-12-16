d42b27efc028c054cc27e217660ce8afeed7879a
Daniel López <daniel.lp1094@gmail.com>
2021-12-16 16:21:47 +0100

    RELEASE: first point completed

--- src/pages/InfoPage/InfoPage.jsx
+++ src/pages/InfoPage/InfoPage.jsx
@@ -4,10 +4,46 @@ import './InfoStyle.scss';
 
 export default function InfoPage() {
 
+    const [info, setInfo] = useState([]);
+
+    /*  const [media, setMedia] = useState();
+     console.log(media) */
+
+    const getInfo = async () => {
+        try {
+            const data = await getAllInfo();
+            setInfo(data.dataseries)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    function findMedia() {
+        let firstTen = info.slice(0, 10);
+        const findSpeed = firstTen.map(items => {
+            return items.wind10m.speed
+        })
+        let sum = 0;
+        for (let i = 0; i < findSpeed.length; i++) {
+            sum += findSpeed[i]
+        }
+        return sum / findSpeed.length;
+    }
+
+    useEffect(() => {
+        getInfo();
+    }, []);
+
 
     return (
         <>
-            
+            <div className='b-card'>
+                <div className='b-content'>
+                    <img className='b-content__img' src="images/logo.png" alt="Logo" />
+                    <p className='b-content__text'>Esta es tu media de los 10 últimos valores de velocidad</p>
+                    <h2 className='b-content__media'>{findMedia().toString()}</h2>
+                </div>
+            </div>
         </>
     )
 }
